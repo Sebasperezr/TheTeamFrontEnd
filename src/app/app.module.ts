55fb59710577cb722ee2aad77f9f185f2656a671
castor-dev3 <alvaro.garcia@castor.com.co>
2020-05-30 15:45:23 -0500

    Feat: empleado
    - Agregar stepper para cuestionario.
    - Agregar imagenes y preguntas.
    - Agregar input para audio.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { EmpleadoComponent } from './pages/empleado/empleado.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmpleadoComponent,
-    InicioComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EmpleadoComponent,
+        InicioComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        NgbModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+        BrowserAnimationsModule,
+        MatStepperModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/empleado/empleado.component.ts
+++ src/app/pages/empleado/empleado.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-empleado',
-  templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.scss']
+    selector: 'app-empleado',
+    templateUrl: './empleado.component.html',
+    styleUrls: ['./empleado.component.scss']
 })
 export class EmpleadoComponent implements OnInit {
 
-  constructor() { }
+    isLinear = false;
+    firstFormGroup: FormGroup;
+    secondFormGroup: FormGroup;
+    numerosArbol = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
-  ngOnInit(): void {
-  }
+    imagenEmociones = './assets/images/arbol.png';
+    imagenMente = './assets/images/mente.png';
+    imagenMente1 = './assets/images/mente1.png';
+    imagenMente2 = './assets/images/mente2.png';
+    imagenMente3 = './assets/images/mente3.png';
+    imagenMente4 = './assets/images/mente4.png';
+    imagenMente5 = './assets/images/mente5.png';
+    imagenMente6 = './assets/images/mente6.png';
+    imagenMente7 = './assets/images/mente7.png';
+    imagenMente8 = './assets/images/mente8.png';
 
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.firstFormGroup = this.formBuilder.group({
+            firstCtrl: ['', Validators.required]
+        });
+        this.secondFormGroup = this.formBuilder.group({
+            secondCtrl: ['', Validators.required]
+        });
+    }
 }
